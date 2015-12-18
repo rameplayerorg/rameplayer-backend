ddcab968c51d92eab4e5a34e66e4dcef6359b6f4
Timo Teräs <timo.teras@iki.fi>
2015-12-18 15:06:45 +0200

    add support for hardware status leds

---
+++ rame-leds.lua
@@ -0,0 +1,28 @@
+local plfile = require 'pl.file'
+local plpath = require 'pl.path'
+local cqueues = require 'cqueues'
+local push = require 'cqp.push'
+local RAME = require 'rame'
+
+local properties = {}
+local mappings = {
+	["rame:green:play"]	= function() local s = RAME.player.status() return s == "playing" or s == "buffering" end,
+	["rame:red:stop"]	= function() return RAME.player.status() == "stopped" end,
+	["rame:yellow:pause"]	= function() return RAME.player.status() == "paused" end,
+}
+
+local Plugin = { }
+function Plugin.init()
+	for led, value in pairs(mappings) do
+		local sysfsled = "/sys/class/leds/"..led
+		if plpath.exists(sysfsled) then
+			local prop = push.computed(value)
+			table.insert(properties, prop)
+			prop:push_to(function(val)
+				plfile.write(sysfsled.."/brightness", val and "255" or "0")
+			end)
+		end
+	end
+end
+
+return Plugin
