08b383856a68cec0c701bf187714277a2918eaf0
Timo Teräs <timo.teras@iki.fi>
2015-12-22 16:25:41 +0200

    event based IP-address updating

--- rameplayer.lua
+++ rameplayer.lua
@@ -4,12 +4,14 @@
 package.path = "/usr/share/rameplayer-backend/?.lua;"..package.path
 
 local posix = require 'posix'
+local socket = require 'socket'
 local pldir = require 'pl.dir'
 local plpath = require 'pl.path'
 local plutils = require 'pl.utils'
 local cqueues = require 'cqueues'
 local httpd = require 'cqp.httpd'
 local process = require 'cqp.process'
+local posixfd = require 'cqp.posixfd'
 local RAME = require 'rame'
 
 -- Clear framebuffer
@@ -88,14 +90,24 @@ local function exit_handler()
 end
 
 local function update_ip()
+	local nlfd, err = posix.socket(posix.AF_NETLINK, posix.SOCK_RAW, posix.NETLINK_ROUTE)
+	posix.bind(nlfd, {family=posix.AF_NETLINK, pid=posix.getpid("pid"), groups=0x440}) --groups=RTMGRP_IPV4_ROUTE|RTMGRP_IPV6_ROUTE
+	local nlsock = posixfd.openfd(nlfd, 'r')
+	local timeout = 0.05
 	while true do
-		local socket = require "socket"
-		local s = socket.udp()
-		s:setpeername("8.8.8.8", 80)
-		local ip, port = s:getsockname()
-		s:close()
-		RAME.system.ip(tostring(ip))
-		cqueues.poll(15.0)
+		if cqueues.poll(nlsock, timeout) == nlsock then
+			-- Just read the netlink data. No real processing,
+			-- instead just refresh the IP.
+			nlsock:read(16*1024)
+			timeout = 0.05
+		else
+			local s = socket.udp()
+			s:setpeername("8.8.8.8", 80)
+			local ip, port = s:getsockname()
+			s:close()
+			RAME.system.ip(tostring(ip))
+			timeout = nil
+		end
 	end
 end
 
