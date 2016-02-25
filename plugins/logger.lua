1aca6ac80e63081db78be80c3a8d91790ca0470a
viliman <viliman.git@gmail.com>
2016-02-25 14:39:16 +0200

    logger POST appends to log file and returns it for each GET
    There is bug in the backend so that it returns for each request one
    byte less the body then what it actually has

--- plugins/logger.lua
+++ plugins/logger.lua
@@ -2,15 +2,31 @@
 local RAME = require 'rame.rame'
 
 local LOG = {}
+local log_f
 
 function LOG.GET(ctx, reply)
-	--ip = ctx.ip
-	local data = { time = "232320", level = "ERROR", message = "Hello Err!" }
+	log_f = io.open("rame_webui_log.txt", "r")
+	if not log_f then
+		return 404, "No log file created!"
+	end
+	local data = log_f:read("*a")
+
+	log_f:close()
+	--print(data)
+	--print(#data)
+	reply.headers["Content-Type"] = "text/plain"
+
 	return 200, data
 end
 
 function LOG.POST(ctx, reply)
-	print(ctx.ip, ctx.args.time, ctx.args.level, ctx.args.message)
+	log_f = io.open("rame_webui_log.txt", "a+")
+	local str = ctx.ip..", "..ctx.args.time..", "..ctx.args.level..", "
+				..ctx.args.message.."\n"
+	print(str)
+	log_f:write(str)
+	log_f:close()
+
 	return 200
 end
 
