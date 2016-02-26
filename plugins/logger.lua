445b5f5a53ac8cf3e9b7211e1cb98548a8d64fda
viliman <viliman.git@gmail.com>
2016-02-26 17:38:41 +0200

    using syslog service for WEBUI logging

--- plugins/logger.lua
+++ plugins/logger.lua
@@ -1,13 +1,19 @@
---local json = require 'cjson.safe'
 local RAME = require 'rame.rame'
+local syslog = require "posix.syslog"
 
 local LOG = {}
-local log_f
+
+local log_levels = {
+	INFO = 6, --LOG_INFO	6
+	DEBUG = 7, --LOG_DEBUG 7
+	WARNING = 4, --LOG_WARNING	4
+	ERROR = 3, --LOG_ERR	3
+}
 
 function LOG.GET(ctx, reply)
-	log_f = io.open("rame_webui_log.txt", "r")
+	log_f = io.open("/var/log/messages", "r")
 	if not log_f then
-		return 404, "No log file created!"
+		return 500
 	end
 	local data = log_f:read("*a")
 
@@ -19,20 +25,18 @@ function LOG.GET(ctx, reply)
 	return 200, data
 end
 
+--todo implement check_fields() checking
 function LOG.POST(ctx, reply)
-	log_f = io.open("rame_webui_log.txt", "a+")
-	local str = ctx.ip..", "..ctx.args.time..", "..ctx.args.level..", "
-				..ctx.args.message.."\n"
-	print(str)
-	log_f:write(str)
-	log_f:close()
-
+	local str = ctx.ip..", "..ctx.args.time..", "..ctx.args.message.."\n"
+	--print(str)
+	syslog.syslog(log_levels[ctx.args.level],str)
 	return 200
 end
 
 local Plugin = {}
 
 function Plugin.init()
+	syslog.openlog("RAME_WEBUI")
 	RAME.rest.log = function(ctx, reply) return ctx:route(reply, LOG) end
 end
 
