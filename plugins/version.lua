0a6c32f329b1679b3a13d27612add34e9b03acb2
viliman <viliman.git@gmail.com>
2016-03-10 19:28:44 +0200

    Includes rame HW board name, product and version info to total hw version info

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,4 +1,6 @@
 local plfile = require 'pl.file'
+local pldir = require 'pl.dir'
+local plpath = require 'pl.path'
 local RAME = require 'rame.rame'
 
 -- REST API: /version/
@@ -16,8 +18,21 @@ end
 local Plugin = {}
 
 function Plugin.init()
-	local hw = plfile.read("/sys/firmware/devicetree/base/model") or ""
-	RAME.version.hardware(hw:sub(1, -2))
+	local str, addon_board_name, addon_board_id, addon_board_ver
+	local base_board_name = plfile.read("/sys/firmware/devicetree/base/model"):sub(1, -2) or ""
+
+	if plpath.exists("/proc/device-tree/rame/product")
+	  and plpath.exists("/proc/device-tree/rame/product_ver")
+ 	  and plpath.exists("/proc/device-tree/rame/product_id") then
+		addon_board = plfile.read("/proc/device-tree/rame/product"):match("[^\n]+") or ""
+		addon_board_id = plfile.read("product_id"):match("[^\n]+") or ""
+		addon_board_ver = plfile.read("/proc/device-tree/rame/product_ver"):match("[^\n]+") or ""
+		RAME.version.hardware(base_board_name.." "..addon_board_name.." "..addon_board_id
+							  .." ("..addon_board_ver..")")
+	else
+		RAME.version.hardware(base_board_name)
+	end
+
 	-- RAME.version.backend is set on build script
 	local firmware = plfile.read("/media/mmcblk0p1/rameversion.txt") or ""
 	RAME.version.firmware(firmware:match("[^\n]+"))
