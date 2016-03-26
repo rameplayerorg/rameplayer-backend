bfe05e9697f18681bb02b719712984f381c5f5f3
Timo Teräs <timo.teras@iki.fi>
2016-03-26 17:10:18 +0200

    improve volume settings saving
    use the property framework to trigger the saving (fixes #26)
    and write it only after there has been no changes for
    some time (fixes #25)

--- plugins/audio-control.lua
+++ plugins/audio-control.lua
@@ -1,5 +1,7 @@
 local RAME = require 'rame.rame'
 local json = require 'cjson.safe'
+local cqueues = require 'cqueues'
+local condition = require 'cqueues.condition'
 
 -- persistant storage for audio volume levels
 local audio_json = "audio.json"
@@ -9,22 +11,20 @@ local AUDIO = { GET = {}, PUT = {} }
 
 local audio_chs = {
 	headphone = {
-		volume = RAME.system.headphone_volume(),
 		min = 0,
 		max = 100,
 		-- db values are only used for sending to web as reference
 		min_db = "-64.0",
 		max_db = "0",
-		func = RAME.system.headphone_volume
+		property = RAME.system.headphone_volume
 	},
 	lineout = {
-		volume = RAME.system.lineout_volume(),
 		min = 0,
 		max = 110,
 		-- db values are only used for sending to web as reference
 		min_db = "-64.0",
 		max_db = "6.4",
-		func = RAME.system.lineout_volume
+		property = RAME.system.lineout_volume
 	}
 }
 
@@ -32,20 +32,17 @@ function AUDIO.GET(ctx, reply)
 	local temp = {}
 
 	for k, v in pairs(audio_chs) do
-		table.insert(temp,
-			{
-				id = k,
-				volume = v.volume,
-				min = v.min,
-				max = v.max,
-				minDb = v.min_db,
-				maxDb = v.max_db
-			})
+		table.insert(temp, {
+			id = k,
+			volume = v.property(),
+			min = v.min,
+			max = v.max,
+			minDb = v.min_db,
+			maxDb = v.max_db
+		})
 	end
 
-	return 200, {
-	  channels = temp
-	}
+	return 200, { channels = temp }
 end
 
 function AUDIO.PUT(ctx, reply)
@@ -70,22 +67,8 @@ function AUDIO.PUT(ctx, reply)
 		local vol = args.volume
 		if vol < channel.min then vol = channel.min end
 		if vol > channel.max then vol = channel.max end
-		channel.volume = vol
 		RAME.log.debug("audio.put "..chn_name.."="..vol)
-		channel.func(vol)
-	end
-
-	local saved = {}
-	for k, v in pairs(audio_chs) do
-		saved[k] = { volume = v.volume }
-	end
-	local saved_json, err = json.encode(saved)
-	--print(saved_json, err)
-
-	-- Store the new audio levels - writing the whole table
-	if saved_json == nil or not RAME.write_settings_file(audio_json, saved_json) then
-		RAME.log.error("File write error: "..audio_json)
-		return 500, { error="File write error: "..audio_json }
+		channel.property(vol)
 	end
 
 	return 200
@@ -93,22 +76,49 @@ end
 
 local Plugin = {}
 
+Plugin.settings_changed = condition.new()
+
 function Plugin.init()
 	-- Read the stored values (or in case of 1st boot "")
 	local audio_conf = json.decode(RAME.read_settings_file(audio_json) or "")
 	if audio_conf ~= nil then
 		for k, v in pairs(audio_chs) do
 			if audio_conf[k] ~= nil then
-				local vol = audio_conf[k].volume
-				v.volume = vol
-				v.func(vol)
-				--print("vol:", k, vol)
+				v.property(audio_conf[k].volume)
 			end
 		end
 	else RAME.log.info("No audio info stored - going with defaults.") end
 
+	for k, v in pairs(audio_chs) do
+		v.property:push_to(function() Plugin.settings_changed:signal() end)
+	end
 
 	RAME.rest.audio = function(ctx, reply) return ctx:route(reply, AUDIO) end
+end
+
+function Plugin.main()
+	local timeout = nil
+	while true do
+		local ret = cqueues.poll(Plugin.settings_changed, timeout)
+		if ret == timeout then
+			timeout = nil
+			RAME.log.info("Storing changed audio levels to disk")
+
+			-- Store the new audio levels - writing the whole table
+			local saved = {}
+			for k, v in pairs(audio_chs) do
+				saved[k] = { volume = v.property() }
+			end
+			local saved_json, err = json.encode(saved)
+			if saved_json == nil or
+			   not RAME.write_settings_file(audio_json, saved_json) then
+				RAME.log.error("Failed to write audio settings: " .. (err or "nil"))
+			end
+		else
+			-- Trigger timeout to write to disk
+			timeout = 10.0
+		end
+	end
 end
 
 return Plugin
