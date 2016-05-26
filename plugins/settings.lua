c645b1b5b30e46ea2bba963a62b298d6c8bc88c1
viliman <viliman.git@gmail.com>
2016-05-26 16:24:02 +0300

    Initial factory reset code

--- plugins/settings.lua
+++ plugins/settings.lua
@@ -486,8 +486,15 @@ function SETTINGS.PUT.reboot(ctx, reply)
 end
 
 function SETTINGS.PUT.reset(ctx, reply)
-	-- TODO - "factory reset" here
-	print("TODO - Factory reset: doing something really drastic here to wipe out everything ;)")
+	process.run("mount", "-o", "remount,rw", "/media/mmcblk0p1")
+	-- all the user specific configuration is erased
+	process.run("rm", "-rf", "/media/mmcblk0p1/user")
+	-- config overlay is destroed
+	process.run("rm", "-rf", "/media/mmcblk0p1/*.apkovl.tar.gz")
+	-- copying the default (factory) settings
+	process.run("cp", "factory.rst", "/media/mmcblk0p1/rame.apkovl.tar.gz")
+	process.run("mount", "-o", "remount,ro", "/media/mmcblk0p1")
+	process.run("reboot", "now")
 	return 200
 end
 
