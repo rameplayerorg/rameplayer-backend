c4b38dbd15639a89e3869f3b595f839d28777866
=Jetro Lauha <jlauha@gmail.com>
2017-05-02 15:00:51 +0300

    list mimetypes accepted by backend for the upload

--- plugins/upload.lua
+++ plugins/upload.lua
@@ -3,9 +3,43 @@ local Item = require 'rame.item'
 
 local BUFFER_SIZE = 64*1024
 
+local accepted_mimetypes = {
+	["video/mp4"] = 1,
+	["video/mpeg"] = 1,
+	["video/x-mpeg"] = 1,
+	["video/quicktime"] = 1,
+	["video/x-matroska"] = 1,
+	["video/x-m4v"] = 1,
+	["video/avi"] = 1,
+	["video/vnd.avi"] = 1,
+	["video/msvideo"] = 1,
+	["video/x-msvideo"] = 1,
+	["video/x-flv"] = 1,
+	["audio/ogg"] = 1,
+	["audio/m4a"] = 1,
+	["audio/x-m4a"] = 1,
+	["audio/aac"] = 1,
+	["audio/flac"] = 1,
+	["audio/mpeg"] = 1,
+	["audio/x-mpeg"] = 1,
+	["audio/mp3"] = 1,
+	["audio/x-mp3"] = 1,
+	["audio/mpeg3"] = 1,
+	["audio/x-mpeg3"] = 1,
+	["audio/wav"] = 1,
+	["audio/x-wav"] = 1,
+	["audio/wave"] = 1,
+	["audio/x-wave"] = 1,
+	["audio/vnd.wave"] = 1,
+	["image/tiff"] = 1,
+	["image/png"] = 1,
+	["image/jpeg"] = 1,
+	["image/gif"] = 1,
+}
+
 -- returns true if upload file is valid
 local function is_valid(filename, mimetype)
-	return mimetype == "video/mp4"
+	return accepted_mimetypes[mimetype]
 end
 
 local UPLOAD = {}
@@ -21,25 +55,27 @@ function UPLOAD.POST(ctx, reply)
 	RAME.log.debug(("user uploading file to %s (%d bytes)"):format(file, ctx.headers["content-length"]))
 	local mimetype = ctx.headers["content-type"]
 	local valid = is_valid(filename, mimetype)
-
-	local mountpoint = RAME.get_mountpoint(file)
-	RAME.remounter:wrap(mountpoint, function()
-		local f = nil
-		if valid then
-			f, err, rc = io.open(file, "wb")
-			if err ~= nil then
-				RAME.log.error(("File write error (%d): %s"):format(rc, err))
+	RAME.log.debug(("MIME type: %s (%s)"):format(mimetype, valid and "accepted" or "NOT ACCEPTED"))
+	
+	if valid then
+		local mountpoint = RAME.get_mountpoint(file)
+		RAME.remounter:wrap(mountpoint, function()
+			local f = nil
+			if valid then
+				f, err, rc = io.open(file, "wb")
+				if err ~= nil then
+					RAME.log.error(("File write error (%d): %s"):format(rc, err))
+				end
 			end
-		end
-		-- read upload file and write it to file
-		ctx.read_file(function(buf)
-			if f then
-				f:write(buf)
-			end
-		end, BUFFER_SIZE)
-		if f ~= nil then f:close() end
-	end)
-	if not valid then
+			-- read upload file and write it to file
+			ctx.read_file(function(buf)
+				if f then
+					f:write(buf)
+				end
+			end, BUFFER_SIZE)
+			if f ~= nil then f:close() end
+		end)
+	else -- not valid:
 		return 400, { error = "Invalid file type" }
 	end
 	return 200, {}
