acee543312730b27956c3ec667a498dd643fefbd
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2017-11-28 13:12:30 +0200

    recorder parameters

--- plugins/recorder.lua
+++ plugins/recorder.lua
@@ -13,7 +13,14 @@ local SCRIPT_PATH = "/etc/bmd-streamer/ffmpeg.sh"
 local SETTINGS_FILE = "recorder.json"
 
 local recorder_fields = {
-	bitrate          = { typeof="number"  },
+	avgVideoBitrate  = { typeof="string"  },
+	maxVideoBitrate  = { typeof="string"  },
+	h264Profile      = { typeof="string"  },
+	h264Level        = { typeof="string"  },
+	h264BFrames      = { typeof="string"  },
+	h264Cabac        = { typeof="string"  },
+	fpsDivider       = { typeof="string"  },
+	audioBitrate     = { typeof="string"  },
 	input            = { typeof="string"  },
 	recorderEnabled  = { typeof="boolean" },
 	recordingPath    = { typeof="string"  },
@@ -80,13 +87,52 @@ local function start_process(cfg)
 				"--syslog",
 				"--firmware-dir", "/etc/bmd-streamer",
 				"--exec", SCRIPT_PATH,
-				"--video-kbps", "" .. (cfg.bitrate - 100),
-				"--video-max-kbps", "" .. cfg.bitrate,
-				"--h264-profile", "main",
-				"--h264-level", "32",
-				"--h264-no-bframes"
 			}
 
+			if cfg.avgVideoBitrate ~= "" then
+				table.insert(cmd, "--video-kbps")
+				table.insert(cmd, "" .. cfg.avgVideoBitrate)
+			end
+
+			if cfg.maxVideoBitrate ~= "" then
+				table.insert(cmd, "--video-max-kbps")
+				table.insert(cmd, "" .. cfg.maxVideoBitrate)
+			end
+
+			if cfg.audioBitrate ~= "" then
+				RAME.log.debug(cfg.audioBitrate)
+				table.insert(cmd, "--audio-kbps")
+				table.insert(cmd, "" .. cfg.audioBitrate)
+			end
+
+			if cfg.h264Profile then
+				table.insert(cmd, "--h264-profile")
+				table.insert(cmd, cfg.h264Profile)
+			end
+
+			if cfg.h264Level ~= "" then
+				table.insert(cmd, "--h264-level")
+				table.insert(cmd, "" .. cfg.h264Level)
+			end
+
+			if cfg.h264BFrames == "enabled" then
+				table.insert(cmd, "--h264-bframes")
+			elseif cfg.h264BFrames == "disabled" then
+				table.insert(cmd, "--h264-no-bframes")
+			end
+
+			if cfg.h264Cabac == "enabled" then
+				table.insert(cmd, "--h264-cabac")
+			elseif cfg.h264Cabac == "disabled" then
+				table.insert(cmd, "--h264-no-cabac")
+			end
+
+			if cfg.fpsDivider ~= nil then
+				table.insert(cmd, "--fps-divider")
+				table.insert(cmd, "" .. cfg.fpsDivider)
+			end
+
+			RAME.log.debug('cmd:', table.unpack(cmd))
 			Plugin.process = process.spawn(table.unpack(cmd))
 
 			-- wait until process is terminated
@@ -112,7 +158,6 @@ local function start_process(cfg)
 
 	-- wait to see if running flag has changed
 	cqueues.poll(WAIT_PROCESS)
-	RAME.log.info('bmd-streamer is running')
 	return RAME.recorder.running()
 end
 
@@ -178,6 +223,23 @@ function Plugin.init()
 	end
 
 	local cfg = json.decode(RAME.read_settings_file(SETTINGS_FILE) or "")
+	if cfg == nil then
+		-- default values
+		cfg = {}
+		cfg.streamingEnabled = false
+		cfg.recorderEnabled = false
+		cfg.input = "sdi"
+		cfg.avgVideoBitrate = "3000"
+		cfg.maxVideoBitrate = "3100"
+		cfg.h264Profile = "main"
+		cfg.h264Level = "32"
+		cfg.h264BFrames = "disabled"
+		cfg.h264Cabac = "default"
+		cfg.fpsDivider = ""
+		cfg.audioBitrate = ""
+		cfg.streamNum = "1"
+		cfg.recordingPath = "/media/sda1/recording.mp4"
+	end
 	if RAME.check_fields(cfg, recorder_fields) == nil then
 		Plugin.settings = cfg
 	end
